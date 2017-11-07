4337e36f6618795c87efd154309163a6cc431ed2
Armand <armand.fourtane@gmail.com>
2017-11-07 15:29:52 +0100

    version graphique V2

--- www/js/app.js
+++ www/js/app.js
@@ -7,8 +7,9 @@
     .run(function($ionicPlatform, $http) {
         $ionicPlatform.ready(function() {
             if(window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+                cordova.plugins.Keyboard.close();
             }
             if(window.StatusBar) {
             StatusBar.styleDefault();
